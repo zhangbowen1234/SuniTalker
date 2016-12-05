66b369964712b8bccf8aeb048c9d14097808d173
fandy <549567567@qq.com>
2016-12-05 18:27:20 +0800

    fenbao

--- chatsdk/src/main/java/com/silver/chatsdk/SSIMClient.java
+++ chatsdk/src/main/java/com/silver/chatsdk/SSIMClient.java
@@ -3,7 +3,7 @@ package com.silver.chatsdk;
 import android.content.Context;
 import android.util.Log;
 
-import com.silver.chatsdk.service.bean.BaseBean;
+import com.silver.chatsdk.service.bean.BaseResponse;
 import com.silver.chatsdk.service.bean.Person;
 import com.silver.chatsdk.service.bean.RequestInfo;
 import com.silver.chatsdk.service.network.ApiService;
@@ -24,17 +24,17 @@ public class SSIMClient {
     private static SSIMClient instance = null;
     private Context mContext;
 
-    /**
-     * 单例模式
-     *
-     * @return 客户端对象
-     */
-    public synchronized static SSIMClient getInstance() {
-        if (instance == null) {
-            instance = new SSIMClient();
-        }
-        return instance;
-    }
+//    /**
+//     * 单例模式
+//     *
+//     * @return 客户端对象
+//     */
+//    public synchronized static SSIMClient getInstance() {
+//        if (instance == null) {
+//            instance = new SSIMClient();
+//        }
+//        return instance;
+//    }
 
     public void init(Context context) {
         mContext = context;
@@ -50,7 +50,7 @@ public class SSIMClient {
             @Override
             public void onResponse(Call<Person> call, Response<Person> response) {
                 Log.e("onResponse", response.body().toString());
-                callBack.onSuccess(new BaseBean(1, "123"));
+                callBack.onSuccess(new BaseResponse(1, "123",123));
             }
 
             @Override
--- chatsdk/src/main/java/com/silver/chatsdk/service/bean/BaseResponse.java
+++ chatsdk/src/main/java/com/silver/chatsdk/service/bean/BaseResponse.java
@@ -5,15 +5,16 @@ package com.silver.chatsdk.service.bean;
  * 邮箱：fandy618@hotmail.com
  */
 
-public class BaseBean {
+public class BaseResponse {
 
     private int code;
     private String errMsg;
+    private long responseTime;
 
-
-    public BaseBean(int code, String errMsg) {
+    public BaseResponse(int code, String errMsg, long responseTime) {
         this.code = code;
         this.errMsg = errMsg;
+        this.responseTime = responseTime;
     }
 
     public int getCode() {
@@ -30,6 +31,14 @@ public class BaseBean {
 
     public void setErrMsg(String errMsg) {
         this.errMsg = errMsg;
+    }
+
+    public long getResponseTime() {
+        return responseTime;
+    }
+
+    public void setResponseTime(long responseTime) {
+        this.responseTime = responseTime;
     }
 }
 
