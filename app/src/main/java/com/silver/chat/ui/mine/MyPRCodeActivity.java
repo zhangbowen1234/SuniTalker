e5e0d62ab666a6ec0f7b9c2feb621f1a1f837dcf
fandy <549567567@qq.com>
2016-12-13 16:42:40 +0800

    增加生成二维码

--- app/src/main/java/com/silver/chat/ui/mine/MyPRCodeActivity.java
+++ app/src/main/java/com/silver/chat/ui/mine/MyPRCodeActivity.java
@@ -1,7 +1,17 @@
 package com.silver.chat.ui.mine;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
 import com.silver.chat.R;
 import com.silver.chat.base.BaseActivity;
+import com.silver.chat.qrcode.core.BGAQRCodeUtil;
+import com.silver.chat.qrcode.zxing.QRCodeEncoder;
 
 /**
  * 作者：Fandy on 2016/12/8 14:54
@@ -10,9 +20,73 @@ import com.silver.chat.base.BaseActivity;
  * 我的二维码
  */
 
-public class MyPRCodeActivity extends BaseActivity{
+public class MyPRCodeActivity extends BaseActivity implements View.OnClickListener {
+    /**
+     * 二维码
+     */
+    private ImageView mIvCode;
+    /**
+     * 扫描
+     */
+    private ImageView mIvScan;
+    /**
+     * 返回
+     */
+    private ImageView mIvBack;
+
     @Override
     protected int getLayoutId() {
         return R.layout.activity_my_pr_code;
+    }
+
+    @Override
+    protected void initView() {
+        super.initView();
+        mIvCode = (ImageView) findViewById(R.id.iv_code);
+        mIvScan = (ImageView) findViewById(R.id.iv_scan);
+        mIvBack = (ImageView) findViewById(R.id.iv_back);
+    }
+
+    @Override
+    protected void initListener() {
+        super.initListener();
+        mIvScan.setOnClickListener(this);
+        mIvBack.setOnClickListener(this);
+    }
+
+    @Override
+    protected void initData() {
+        super.initData();
+        new CodeAsyncTask().execute();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.iv_scan:
+
+                break;
+            case R.id.iv_back:
+                finish();
+                break;
+        }
+    }
+
+    class CodeAsyncTask extends AsyncTask<Void,Void,Bitmap>{
+
+        @Override
+        protected Bitmap doInBackground(Void... voids) {
+            Bitmap logoBitmap = BitmapFactory.decodeResource(MyPRCodeActivity.this.getResources(), R.mipmap.ic_launcher);
+            return QRCodeEncoder.syncEncodeQRCode("IM", BGAQRCodeUtil.dp2px(MyPRCodeActivity.this, 244), Color.BLACK, Color.WHITE, logoBitmap);
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            if (bitmap != null) {
+                mIvCode.setImageBitmap(bitmap);
+            } else {
+                Toast.makeText(MyPRCodeActivity.this, "生成带logo的二维码失败", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
