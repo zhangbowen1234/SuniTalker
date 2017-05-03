0c1ccbcc983e87ea7e8a34d1ffa4082fa893f517
unknown <>
2017-05-03 17:29:40 +0800

    我的界面主色调更改仅在当前界面

--- app/src/main/java/com/silver/chat/ui/mine/setting/ChangeBackgroundActivity.java
+++ app/src/main/java/com/silver/chat/ui/mine/setting/ChangeBackgroundActivity.java
@@ -1,58 +1,111 @@
 package com.silver.chat.ui.mine.setting;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.silver.chat.R;
 import com.silver.chat.base.BaseActivity;
+import com.silver.chat.util.PreferenceUtil;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * Created by bowen on 2017/4/28.
  */
 
 public class ChangeBackgroundActivity extends BaseActivity {
-    Button black_theme,blue_theme,green_theme,red_theme,purple_theme;
-
-    @Override
-    protected void initView() {
-        super.initView();
-        black_theme = (Button) findViewById(R.id.black_theme);
-        blue_theme = (Button) findViewById(R.id.blue_theme);
-        green_theme = (Button) findViewById(R.id.green_theme);
-        red_theme = (Button) findViewById(R.id.red_theme);
-        purple_theme = (Button) findViewById(R.id.purple_theme);
-
-    }
+    @BindView(R.id.black_choose)
+    ImageView blackChoose;
+    @BindView(R.id.black_theme)
+    LinearLayout blackTheme;
+    @BindView(R.id.blue_choose)
+    ImageView blueChoose;
+    @BindView(R.id.blue_theme)
+    LinearLayout blueTheme;
+    @BindView(R.id.green_choose)
+    ImageView greenChoose;
+    @BindView(R.id.green_theme)
+    LinearLayout greenTheme;
+    @BindView(R.id.red_choose)
+    ImageView redChoose;
+    @BindView(R.id.red_theme)
+    LinearLayout redTheme;
+    @BindView(R.id.purple_choose)
+    ImageView purpleChoose;
+    @BindView(R.id.purple_theme)
+    LinearLayout purpleTheme;
+    @BindView(R.id.preservation_theme)
+    ImageView preservationTheme;
+    @BindView(R.id.ll_change_background)
+    LinearLayout llChangeBackground;
 
     @Override
     protected int getLayoutId() {
         return R.layout.acyivity_change_background;
     }
 
-    private void onClick(View view){
-        switch (view.getId()){
-            case R.id.black_theme:
 
+    @OnClick({R.id.black_theme, R.id.blue_theme, R.id.green_theme, R.id.red_theme, R.id.purple_theme,R.id.preservation_theme})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.black_theme:
+                BaseActivity.getBackgroundView().setBackgroundDrawable(this.getResources().getDrawable(R.drawable.black_theme));
+                blackChoose.setVisibility(View.VISIBLE);
+                blueChoose.setVisibility(View.INVISIBLE);
+                greenChoose.setVisibility(View.INVISIBLE);
+                redChoose.setVisibility(View.INVISIBLE);
+                purpleChoose.setVisibility(View.INVISIBLE);
                 break;
             case R.id.blue_theme:
+                BaseActivity.getBackgroundView().setBackgroundDrawable(this.getResources().getDrawable(R.drawable.blue_theme));
+                blackChoose.setVisibility(View.INVISIBLE);
+                blueChoose.setVisibility(View.VISIBLE);
+                greenChoose.setVisibility(View.INVISIBLE);
+                redChoose.setVisibility(View.INVISIBLE);
+                purpleChoose.setVisibility(View.INVISIBLE);
 
                 break;
 
             case R.id.green_theme:
-
+                BaseActivity.getBackgroundView().setBackgroundDrawable(this.getResources().getDrawable(R.drawable.green_theme));
+                greenChoose.setVisibility(View.VISIBLE);
+                blackChoose.setVisibility(View.INVISIBLE);
+                blueChoose.setVisibility(View.INVISIBLE);
+                redChoose.setVisibility(View.INVISIBLE);
+                purpleChoose.setVisibility(View.INVISIBLE);
                 break;
 
             case R.id.red_theme:
-
+                BaseActivity.getBackgroundView().setBackgroundDrawable(this.getResources().getDrawable(R.drawable.red_theme));
+                redChoose.setVisibility(View.VISIBLE);
+                blackChoose.setVisibility(View.INVISIBLE);
+                blueChoose.setVisibility(View.INVISIBLE);
+                greenChoose.setVisibility(View.INVISIBLE);
+                purpleChoose.setVisibility(View.INVISIBLE);
                 break;
 
             case R.id.purple_theme:
-
+                BaseActivity.getBackgroundView().setBackgroundDrawable(this.getResources().getDrawable(R.drawable.purple_theme));
+                purpleChoose.setVisibility(View.VISIBLE);
+                blackChoose.setVisibility(View.INVISIBLE);
+                blueChoose.setVisibility(View.INVISIBLE);
+                greenChoose.setVisibility(View.INVISIBLE);
+                redChoose.setVisibility(View.INVISIBLE);
+                break;
+            case R.id.preservation_theme:
+//                PreferenceUtil.getInstance(mContext).setLog(true);
+                finish();
                 break;
 
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
 
     }
 }
