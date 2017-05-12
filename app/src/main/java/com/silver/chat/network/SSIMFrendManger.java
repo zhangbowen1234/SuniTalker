7d3f7ab56b90243b539e6d0e8f1c9e0c06051768
joe <maqiao@silversecuritytech.com>
2017-05-12 11:37:15 +0800

    网络请求的回调方法抽取

--- app/src/main/java/com/silver/chat/network/SSIMFrendManger.java
+++ app/src/main/java/com/silver/chat/network/SSIMFrendManger.java
@@ -13,6 +13,8 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
+
 /**
  * Created by hibon on 2017/5/8.
  * 与个人信息以及个人好友信息的网络请求
@@ -20,65 +22,75 @@ import retrofit2.Response;
 
 public class SSIMFrendManger {
 
-
-    /**
-     * 获取联系人列表
-     * @param version
-     * @param userId 当前用户id
-     * @param page 请求页数(从0开始)
-     * @param count 每页显示条数
-     * @param token
-     * @param callBack
-     */
-    public static void contactList(String version, String userId, String page, String count,String token ,
-                                   final ResponseCallBack<BaseResponse<ArrayList<ContactListBean>>> callBack){
-        ApiService imApi = RetrofitHelper.create().imApi;
-        Call<BaseResponse<ArrayList<ContactListBean>>> baseResponseCall = imApi.contactList(version,userId,page,count,token);
-        baseResponseCall.enqueue(new Callback<BaseResponse<ArrayList<ContactListBean>>>() {
+    public static <T> void enqueue(Call<BaseResponse<T>> baseResponseCall, final ResponseCallBack<BaseResponse<T>> callBack) {
+        baseResponseCall.enqueue(new Callback<BaseResponse<T>>() {
             @Override
-            public void onResponse(Call<BaseResponse<ArrayList<ContactListBean>>> call, Response<BaseResponse<ArrayList<ContactListBean>>> response) {
-                Log.e("contactList",response.body()+"");
+            public void onResponse(Call<BaseResponse<T>> call, Response<BaseResponse<T>> response) {
+                Log.e("contactList", response.body() + "");
 
-                if (response.body().getStatusCode() == 200){
+                if (response.body().getStatusCode() == 200) {
                     callBack.onSuccess(response.body());
-                }else{
+                } else if(response.body().getStatusCode() == 300){
+
+
+                }else {
                     callBack.onFailed(response.body());
+
                 }
             }
 
             @Override
-            public void onFailure(Call<BaseResponse<ArrayList<ContactListBean>>> call, Throwable t) {
-                Log.e("aaa", t.toString() );
+            public void onFailure(Call<BaseResponse<T>> call, Throwable t) {
+                Log.e("aaa", t.toString());
                 callBack.onError();
             }
         });
     }
 
+    /**
+     * 获取联系人列表
+     *
+     * @param version
+     * @param userId   当前用户id
+     * @param page     请求页数(从0开始)
+     * @param count    每页显示条数
+     * @param token
+     * @param callBack
+     */
+    public static void contactList(String version, String userId, String page, String count, String token,
+                                   final ResponseCallBack<BaseResponse<ArrayList<ContactListBean>>> callBack) {
+        ApiService imApi = RetrofitHelper.create().imApi;
+        Call<BaseResponse<ArrayList<ContactListBean>>> baseResponseCall = imApi.contactList(version, userId, page, count, token);
+        enqueue(baseResponseCall,callBack);
+    }
+
 
     /**
      * 申请添加好友
+     *
      * @param userId
      * @param friendId
      * @param comment
      * @param token
      * @param callBack
      */
-    public static void goAddFriends(String userId,String friendId,String comment,String token,final ResponseCallBack<BaseResponse> callBack){
+    public static void goAddFriends(String userId, String friendId, String comment, String token, final ResponseCallBack<BaseResponse> callBack) {
         ApiService imApi = RetrofitHelper.create().imApi;
         Call<BaseResponse> baseResponseCall = imApi.addFriend(userId, friendId, comment, token);
         baseResponseCall.enqueue(new Callback<BaseResponse>() {
             @Override
             public void onResponse(Call<BaseResponse> call, Response<BaseResponse> response) {
-                Log.e("addFriends",response.toString()+"");
-                if (response.body().getStatusCode() == 200){
+                Log.e("addFriends", response.toString() + "");
+                if (response.body().getStatusCode() == 200) {
                     callBack.onSuccess(response.body());
 
-                }else{
+                } else {
                     callBack.onFailed(response.body());
                 }
                 Log.e("response", response.body().toString());
 
             }
+
             @Override
             public void onFailure(Call<BaseResponse> call, Throwable t) {
                 callBack.onError();
@@ -88,6 +100,7 @@ public class SSIMFrendManger {
 
     /**
      * 搜素用户
+     *
      * @param type
      * @param condition
      * @param page
@@ -95,16 +108,16 @@ public class SSIMFrendManger {
      * @param token
      * @param callBack
      */
-    public static void searchUser(String type,String condition,String page,String count,String token,final ResponseCallBack<BaseResponse<ArrayList<SearchIdBean>>> callBack){
+    public static void searchUser(String type, String condition, String page, String count, String token, final ResponseCallBack<BaseResponse<ArrayList<SearchIdBean>>> callBack) {
         ApiService imApi = RetrofitHelper.create().imApi;
-        Call<BaseResponse<ArrayList<SearchIdBean>>> baseResponseCall = imApi.searchUser(type, condition, page,count, token);
+        Call<BaseResponse<ArrayList<SearchIdBean>>> baseResponseCall = imApi.searchUser(type, condition, page, count, token);
         baseResponseCall.enqueue(new Callback<BaseResponse<ArrayList<SearchIdBean>>>() {
             @Override
             public void onResponse(Call<BaseResponse<ArrayList<SearchIdBean>>> call, Response<BaseResponse<ArrayList<SearchIdBean>>> response) {
-                Log.e("searchUser",response.body()+"");
-                if (response.body().getStatusCode() == 200){
+                Log.e("searchUser", response.body() + "");
+                if (response.body().getStatusCode() == 200) {
                     callBack.onSuccess(response.body());
-                }else{
+                } else {
                     callBack.onFailed(response.body());
                 }
             }
--- app/src/main/java/com/silver/chat/network/SSIMLoginManger.java
+++ app/src/main/java/com/silver/chat/network/SSIMLoginManger.java
@@ -29,239 +29,157 @@ public class SSIMLoginManger {
 
     /**
      * 验证手机号是否注册
-     * @param version 版本号当前为leaf
-     * @param phone 手机号
+     *
+     * @param version  版本号当前为leaf
+     * @param phone    手机号
      * @param callBack
      */
-    public static void checkPhone(String version,String phone,final ResponseCallBack<BaseResponse> callBack) {
+    public static void checkPhone(String version, String phone, final ResponseCallBack<BaseResponse> callBack) {
         ApiService imApi = RetrofitHelper.create().imApi;
-        Call<BaseResponse> baseResponseCall = imApi.checkPhoneCode(version,phone);
-        baseResponseCall.enqueue(new Callback<BaseResponse>() {
-            @Override
-            public void onResponse(Call<BaseResponse> call, Response<BaseResponse> response) {
-                if (response.body().getStatusCode() == 200 || response.body().getStatusCode() == 2) {
-                    callBack.onSuccess(response.body());
-                } else {
-                    callBack.onFailed(response.body());
-                }
-                //Log.e("response", response.body().toString());
-            }
+        Call<BaseResponse> baseResponseCall = imApi.checkPhoneCode(version, phone);
+        enqueue(baseResponseCall, callBack);
 
-            @Override
-            public void onFailure(Call<BaseResponse> call, Throwable t) {
-                callBack.onError();
-            }
-        });
     }
+
     /**
      * 发送验证码
-     * @param version 版本号当前为leaf
-     * @param phone 手机号
-     * @param genre 请求种类
+     *
+     * @param version  版本号当前为leaf
+     * @param phone    手机号
+     * @param genre    请求种类
      * @param callBack 请求回调
      */
-    public static void userReginstCode(String version,String phone,int genre,final ResponseCallBack<BaseResponse> callBack) {
+    public static void userReginstCode(String version, String phone, int genre, final ResponseCallBack<BaseResponse> callBack) {
         ApiService imApi = RetrofitHelper.create().imApi;
-        Call<BaseResponse> baseResponseCall = imApi.registCode(version,phone,genre);
-        baseResponseCall.enqueue(new Callback<BaseResponse>() {
-            @Override
-            public void onResponse(Call<BaseResponse> call, Response<BaseResponse> response) {
-                if (response.body().getStatusCode() == 200){
-                    callBack.onSuccess(response.body());
-                }else{
-                    callBack.onFailed(response.body());
-                }
-                //Log.e("RegisterPhoneActivity", response.body().toString() );
-            }
-            @Override
-            public void onFailure(Call<BaseResponse> call, Throwable t) {
-                callBack.onError();
-            }
-        });
+        Call<BaseResponse> baseResponseCall = imApi.registCode(version, phone, genre);
+        enqueue(baseResponseCall, callBack);
+
     }
 
     /**
      * 注册
+     *
      * @param version
-     * @param request 注册对象
+     * @param request  注册对象
      * @param callBack
      */
     public static void goReginst(String version, RegisterRequest request, final ResponseCallBack<BaseResponse> callBack) {
         ApiService imApi = RetrofitHelper.create().imApi;
-        Call<BaseResponse> baseResponseCall = imApi.registPhone(version,request);
-        baseResponseCall.enqueue(new Callback<BaseResponse>() {
-            @Override
-            public void onResponse(Call<BaseResponse> call, Response<BaseResponse> response) {
-                if (response.body().getStatusCode() == 200){
-                    callBack.onSuccess(response.body());
+        Call<BaseResponse> baseResponseCall = imApi.registPhone(version, request);
+        enqueue(baseResponseCall, callBack);
 
-                }else{
-                    callBack.onFailed(response.body());
-                }
-                Log.e("response", response.body().toString());
-
-            }
-            @Override
-            public void onFailure(Call<BaseResponse> call, Throwable t) {
-                callBack.onError();
-            }
-        });
     }
 
     /**
      * 登录
+     *
      * @param version
      * @param loginRequest 登录对象
      * @param callBack
      */
     public static void goLogin(String version, LoginRequest loginRequest, final ResponseCallBack<BaseResponse<LoginRequestBean>> callBack) {
         ApiService imApi = RetrofitHelper.create().imApi;
-        Call<BaseResponse<LoginRequestBean>> baseResponseCall = imApi.goLogin(version,loginRequest);
-        baseResponseCall.enqueue(new Callback<BaseResponse<LoginRequestBean>>() {
-            @Override
-            public void onResponse(Call<BaseResponse<LoginRequestBean>> call, Response<BaseResponse<LoginRequestBean>> response) {
-                if (response.body().getStatusCode() == 200){
-                    callBack.onSuccess(response.body());
-                }else{
-                    callBack.onFailed(response.body());
-                }
-            }
+        Call<BaseResponse<LoginRequestBean>> baseResponseCall = imApi.goLogin(version, loginRequest);
+        enqueue(baseResponseCall, callBack);
 
-            @Override
-            public void onFailure(Call<BaseResponse<LoginRequestBean>> call, Throwable t) {
-                callBack.onError();
-            }
-        });
     }
 
 
     /**
      * 获取用户信息
+     *
      * @param version
      * @param token
      * @param callBack
      */
-    public static void getUserInfo(String version, String token, final ResponseCallBack<BaseResponse<UserInfoBean>> callBack){
+    public static void getUserInfo(String version, String token, final ResponseCallBack<BaseResponse<UserInfoBean>> callBack) {
         ApiService imApi = RetrofitHelper.create().imApi;
         Call<BaseResponse<UserInfoBean>> baseResponseCall = imApi.userInfo(version, token);
-        baseResponseCall.enqueue(new Callback<BaseResponse<UserInfoBean>>() {
-            @Override
-            public void onResponse(Call<BaseResponse<UserInfoBean>> call, Response<BaseResponse<UserInfoBean>> response) {
-                if (response.body().getStatusCode() == 200){
-                    callBack.onSuccess(response.body());
-                }else{
-                    callBack.onFailed(response.body());
-                }
-            }
+        enqueue(baseResponseCall, callBack);
 
-            @Override
-            public void onFailure(Call<BaseResponse<UserInfoBean>> call, Throwable t) {
-                callBack.onError();
-            }
-        });
     }
 
     /**
      * 获取好友信息
+     *
      * @param version
-     * @param userId 当前用户id
+     * @param userId   当前用户id
      * @param friendid 好友id
      * @param token
      * @param callBack
      */
-    public static void friendinfo(String token,String version, String userId, String friendid,
-                                   final ResponseCallBack<BaseResponse<List<FriendInfo>>> callBack){
+    public static void friendinfo(String token, String version, String userId, String friendid,
+                                  final ResponseCallBack<BaseResponse<List<FriendInfo>>> callBack) {
         ApiService imApi = RetrofitHelper.create().imApi;
-        Call<BaseResponse<List<FriendInfo>>> baseResponseCall = imApi.friendinfo(token,version,userId,friendid);
-        baseResponseCall.enqueue(new Callback<BaseResponse<List<FriendInfo>>>() {
-            @Override
-            public void onResponse(Call<BaseResponse<List<FriendInfo>>> call, Response<BaseResponse<List<FriendInfo>>> response) {
-                if (response.body().getStatusCode() == 200){
-                    callBack.onSuccess(response.body());
-                }else{
-                    callBack.onFailed(response.body());
-                }
-            }
+        Call<BaseResponse<List<FriendInfo>>> baseResponseCall = imApi.friendinfo(token, version, userId, friendid);
+        enqueue(baseResponseCall, callBack);
 
-            @Override
-            public void onFailure(Call<BaseResponse<List<FriendInfo>>> call, Throwable t) {
-                callBack.onError();
-            }
-        });
     }
+
     /**
      * 退出登录
+     *
      * @param version
      * @param token
      * @param callBack
      */
     public static void outLogin(String version, String token, final ResponseCallBack<BaseResponse> callBack) {
         ApiService imApi = RetrofitHelper.create().imApi;
-        Call<BaseResponse> baseResponseCall = imApi.outLogin(version,token);
-        baseResponseCall.enqueue(new Callback<BaseResponse>() {
-            @Override
-            public void onResponse(Call<BaseResponse> call, Response<BaseResponse> response) {
-                if (response.body().getStatusCode() == 200){
-                    callBack.onSuccess(response.body());
-                }else{
-                    callBack.onFailed(response.body());
-                }
-            }
+        Call<BaseResponse> baseResponseCall = imApi.outLogin(version, token);
+        enqueue(baseResponseCall, callBack);
 
-            @Override
-            public void onFailure(Call<BaseResponse> call, Throwable t) {
-                callBack.onError();
-            }
-        });
     }
+
     /**
      * 修改好友信息
+     *
      * @param version
      * @param userInfoBean 实体类
      * @param token
      * @param callBack
      */
-    public static void updateUserInfo(String version, String token, UpdateUserInfoBean userInfoBean, final ResponseCallBack<BaseResponse<UpdateUserInfoBean>> callBack){
+    public static void updateUserInfo(String version, String token, UpdateUserInfoBean userInfoBean, final ResponseCallBack<BaseResponse<UpdateUserInfoBean>> callBack) {
         ApiService imApi = RetrofitHelper.create().imApi;
-        Call<BaseResponse<UpdateUserInfoBean>> baseResponseCall = imApi.updateInfo(version,token,userInfoBean);
-        baseResponseCall.enqueue(new Callback<BaseResponse<UpdateUserInfoBean>>() {
-            @Override
-            public void onResponse(Call<BaseResponse<UpdateUserInfoBean>> call, Response<BaseResponse<UpdateUserInfoBean>> response) {
-                if (response.body().getStatusCode() == 200){
-                    callBack.onSuccess(response.body());
-                }else{
-                    callBack.onFailed(response.body());
-                }
-            }
+        Call<BaseResponse<UpdateUserInfoBean>> baseResponseCall = imApi.updateInfo(version, token, userInfoBean);
+        enqueue(baseResponseCall, callBack);
 
-            @Override
-            public void onFailure(Call<BaseResponse<UpdateUserInfoBean>> call, Throwable t) {
-
-            }
-        });
     }
+
     /**
      * 忘记密码
+     *
      * @param version
      * @param forgetPasswordBean 实体类
      * @param callBack
      */
-    public static void forgetpwd(String version, ForgetPasswordBean forgetPasswordBean, final ResponseCallBack<BaseResponse<ForgetPasswordBean>> callBack){
+    public static void forgetpwd(String version, ForgetPasswordBean forgetPasswordBean, final ResponseCallBack<BaseResponse<ForgetPasswordBean>> callBack) {
         ApiService imApi = RetrofitHelper.create().imApi;
-        Call<BaseResponse<ForgetPasswordBean>> baseResponseCall = imApi.backpassword(version,forgetPasswordBean);
-        baseResponseCall.enqueue(new Callback<BaseResponse<ForgetPasswordBean>>() {
+        Call<BaseResponse<ForgetPasswordBean>> baseResponseCall = imApi.backpassword(version, forgetPasswordBean);
+        enqueue(baseResponseCall, callBack);
+
+    }
+
+    public static <T> void enqueue(Call<BaseResponse<T>> baseResponseCall, final ResponseCallBack<BaseResponse<T>> callBack) {
+        baseResponseCall.enqueue(new Callback<BaseResponse<T>>() {
             @Override
-            public void onResponse(Call<BaseResponse<ForgetPasswordBean>> call, Response<BaseResponse<ForgetPasswordBean>> response) {
-                if (response.body().getStatusCode() == 200){
+            public void onResponse(Call<BaseResponse<T>> call, Response<BaseResponse<T>> response) {
+                Log.e("contactList", response.body() + "");
+
+                if (response.body().getStatusCode() == 200) {
                     callBack.onSuccess(response.body());
-                }else{
+                } else if (response.body().getStatusCode() == 300) {
+
+
+                } else {
                     callBack.onFailed(response.body());
+
                 }
             }
 
             @Override
-            public void onFailure(Call<BaseResponse<ForgetPasswordBean>> call, Throwable t) {
-
+            public void onFailure(Call<BaseResponse<T>> call, Throwable t) {
+                Log.e("aaa", t.toString());
+                callBack.onError();
             }
         });
     }
