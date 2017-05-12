0a4f2d3d38b8d8435464e1ccfa7170e700e99924
bowen <zhangbowen@silversecuritytech.com>
2017-05-12 17:37:53 +0800

    重构mianactivity

--- app/src/main/java/com/silver/chat/MainActivity.java
+++ app/src/main/java/com/silver/chat/MainActivity.java
@@ -1,5 +1,7 @@
 package com.silver.chat;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -13,34 +15,68 @@ import com.silver.chat.adapter.MainPagerAdapter;
 import com.silver.chat.base.BaseActivity;
 import com.silver.chat.ui.chat.SearchChatRecordActivity;
 import com.silver.chat.ui.contact.AddFriendActivity;
-import com.silver.chat.ui.contact.group.CreatGroupActivity;
 import com.silver.chat.ui.contact.SearchContactActivity;
+import com.silver.chat.ui.contact.group.CreatGroupActivity;
 import com.silver.chat.ui.mine.ScanActivity;
 import com.silver.chat.ui.mine.SettingActivity;
+import com.silver.chat.util.ScreenManager;
 import com.silver.chat.util.ToastUtils;
 import com.silver.chat.view.BadgedTabCustomView;
 import com.silver.chat.view.TabLayoutPlus;
 
-import static com.silver.chat.R.id.tabLayout;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
-public class MainActivity extends BaseActivity implements View.OnClickListener {
+public class MainActivity extends BaseActivity {
 
-    private Toolbar mToolbar;
-    private ViewPager mViewPager;
-    private TabLayoutPlus mTabLayout;
+    @BindView(R.id.iv_toolbar_search)
+    ImageView ivToolbarSearch;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.tabLayout)
+    TabLayoutPlus tabLayout;
+    @BindView(R.id.viewpager)
+    ViewPager viewpager;
+    @BindView(R.id.iv_more)
+    ImageView ivMore;
+    @BindView(R.id.tv_scan)
+    TextView tvScan;
+    @BindView(R.id.tv_search)
+    TextView tvSearch;
+    @BindView(R.id.tv_start_chat)
+    TextView tvStartChat;
+    @BindView(R.id.tv_unread)
+    TextView tvUnread;
+    @BindView(R.id.tv_read)
+    TextView tvRead;
+    @BindView(R.id.tv_clear)
+    TextView tvClear;
+    @BindView(R.id.iv_back)
+    ImageView ivBack;
+    @BindView(R.id.lly_more)
+    RelativeLayout llyMore;
+    @BindView(R.id.tv_scan_chat)
+    TextView tvScanChat;
+    @BindView(R.id.tv_search_chat)
+    TextView tvSearchChat;
+    @BindView(R.id.tv_start_group_chat)
+    TextView tvStartGroupChat;
+    @BindView(R.id.tv_add_friend)
+    TextView tvAddFriend;
+    @BindView(R.id.tv_search_group)
+    TextView tvSearchGroup;
+    @BindView(R.id.tv_more_chat)
+    TextView tvMoreChat;
+    @BindView(R.id.iv_back_chat)
+    ImageView ivBackChat;
+    @BindView(R.id.lly_chat_more)
+    RelativeLayout llyChatMore;
+
+
     private MainPagerAdapter mPagerAdapter;
-    private ImageView mIvSearch;
-    private ImageView mIvMore;
     private Animation mButtonInAnimation;
     private Animation mButtonOutAnimation;
-    private RelativeLayout mLlyMore,mLlyChatMore;
-    private TextView mTvScan,mTvChatScan;
-    private TextView mTvSearch,mTvChatSearch;
-    private TextView mTvStartChat,mTvAddGroup;
-    private TextView mTvUnRead,mTvAddFriend;
-    private TextView mTvRead,mTvSearchGroup;
-    private TextView mTvClear,mTvChatMore;
-    private ImageView mIvBack,mIvMoreBack;
 
     @Override
     protected int getLayoutId() {
@@ -50,108 +86,31 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     @Override
     protected void initView() {
         super.initView();
-        mToolbar = (Toolbar) findViewById(R.id.toolbar);
-        mViewPager = (ViewPager) findViewById(R.id.viewpager);
-        mTabLayout = (TabLayoutPlus) findViewById(tabLayout);
-        mIvSearch = (ImageView) findViewById(R.id.iv_toolbar_search);
-
-        mLlyChatMore = (RelativeLayout) findViewById(R.id.lly_chat_more);
-        mTvChatScan = (TextView) findViewById(R.id.tv_scan_chat);
-        mTvChatSearch = (TextView) findViewById(R.id.tv_search_chat);
-        mTvAddFriend = (TextView) findViewById(R.id.tv_add_friend);
-        mTvAddGroup = (TextView) findViewById(R.id.tv_start_group_chat);
-        mTvSearchGroup = (TextView) findViewById(R.id.tv_search_group);
-        mTvChatMore = (TextView) findViewById(R.id.tv_more_chat);
-        mIvMoreBack = (ImageView) findViewById(R.id.iv_back_chat);
-
-        mLlyMore = (RelativeLayout) findViewById(R.id.lly_more);
-        mIvMore = (ImageView) findViewById(R.id.iv_more);
-        mTvScan = (TextView) findViewById(R.id.tv_scan);
-        mTvSearch = (TextView) findViewById(R.id.tv_search);
-        mTvStartChat = (TextView) findViewById(R.id.tv_start_chat);
-        mTvUnRead = (TextView) findViewById(R.id.tv_unread);
-        mTvRead = (TextView) findViewById(R.id.tv_read);
-        mTvClear = (TextView) findViewById(R.id.tv_clear);
-        mIvBack = (ImageView) findViewById(R.id.iv_back);
         mButtonInAnimation = AnimationUtils.loadAnimation(this, R.anim.button_in);
         mButtonOutAnimation = AnimationUtils.loadAnimation(this, R.anim.button_out);
-        setSupportActionBar(mToolbar);
-        mToolbar.setTitle("chat");
+        setSupportActionBar(toolbar);
+        toolbar.setTitle("chat");
     }
 
     @Override
     protected void initData() {
         super.initData();
         mPagerAdapter = new MainPagerAdapter(getSupportFragmentManager());
-        mViewPager.setOffscreenPageLimit(3);
-        mViewPager.setAdapter(mPagerAdapter);
-        mTabLayout.setupWithViewPager(mViewPager);
-        for (int i = 0; i < mTabLayout.getTabCount(); i++) {
-            BadgedTabCustomView customView = mTabLayout.getTabCustomViewAt(i);
+        viewpager.setOffscreenPageLimit(3);
+        viewpager.setAdapter(mPagerAdapter);
+        tabLayout.setupWithViewPager(viewpager);
+        for (int i = 0; i < tabLayout.getTabCount(); i++) {
+            BadgedTabCustomView customView = tabLayout.getTabCustomViewAt(i);
             if (customView != null) {
                 customView.setTabCount(true, i);
             }
         }
-        mTabLayout.getTabCustomViewAt(1).setTabCount(true, 0);
-    }
-
-    private void openPanelView() {
-        if (mTabLayout.getSelectedTabPosition() == 0){
-            mLlyMore.setVisibility(View.VISIBLE);
-            mTvScan.startAnimation(mButtonInAnimation);
-            mTvSearch.startAnimation(mButtonInAnimation);
-            mTvStartChat.startAnimation(mButtonInAnimation);
-            mTvRead.startAnimation(mButtonInAnimation);
-            mTvUnRead.startAnimation(mButtonInAnimation);
-            mTvClear.startAnimation(mButtonInAnimation);
-        } else if (mTabLayout.getSelectedTabPosition() == 1) {
-            mLlyChatMore.setVisibility(View.VISIBLE);
-            mTvChatScan.startAnimation(mButtonInAnimation);
-            mTvChatSearch.startAnimation(mButtonInAnimation);
-            mTvAddGroup.startAnimation(mButtonInAnimation);
-            mTvAddFriend.startAnimation(mButtonInAnimation);
-            mTvSearchGroup.startAnimation(mButtonInAnimation);
-            mTvChatMore.startAnimation(mButtonInAnimation);
-        }
-    }
-
-    private void closePanelView() {
-        if (mTabLayout.getSelectedTabPosition() == 0){
-            mTvScan.startAnimation(mButtonOutAnimation);
-            mTvSearch.startAnimation(mButtonOutAnimation);
-            mTvStartChat.startAnimation(mButtonOutAnimation);
-            mTvRead.startAnimation(mButtonOutAnimation);
-            mTvUnRead.startAnimation(mButtonOutAnimation);
-            mTvClear.startAnimation(mButtonOutAnimation);
-        } else if (mTabLayout.getSelectedTabPosition() == 1) {
-            mTvChatScan.startAnimation(mButtonOutAnimation);
-            mTvChatSearch.startAnimation(mButtonOutAnimation);
-            mTvAddGroup.startAnimation(mButtonOutAnimation);
-            mTvAddFriend.startAnimation(mButtonOutAnimation);
-            mTvSearchGroup.startAnimation(mButtonOutAnimation);
-            mTvChatMore.startAnimation(mButtonOutAnimation);
-        }
+        tabLayout.getTabCustomViewAt(1).setTabCount(true, 0);
     }
 
     @Override
     protected void initListener() {
         super.initListener();
-        mIvMore.setOnClickListener(this);
-        mIvSearch.setOnClickListener(this);
-        mIvBack.setOnClickListener(this);
-        mTvScan.setOnClickListener(this);
-        mTvStartChat.setOnClickListener(this);
-        mTvRead.setOnClickListener(this);
-        mTvUnRead.setOnClickListener(this);
-        mTvClear.setOnClickListener(this);
-
-        mTvChatScan.setOnClickListener(this);
-        mTvChatSearch.setOnClickListener(this);
-        mTvAddGroup.setOnClickListener(this);
-        mTvAddFriend.setOnClickListener(this);
-        mTvSearchGroup.setOnClickListener(this);
-        mIvMoreBack.setOnClickListener(this);
-        mTvChatMore.setOnClickListener(this);
         mButtonOutAnimation.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
@@ -160,8 +119,8 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
 
             @Override
             public void onAnimationEnd(Animation animation) {
-                mLlyMore.setVisibility(View.GONE);
-                mLlyChatMore.setVisibility(View.GONE);
+                llyMore.setVisibility(View.GONE);
+                llyChatMore.setVisibility(View.GONE);
             }
 
             @Override
@@ -169,7 +128,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
 
             }
         });
-        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+        viewpager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
@@ -187,21 +146,22 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
 
     }
 
-    @Override
-    public void onClick(View view) {
+    //更多弹窗点击
+    @OnClick({R.id.iv_toolbar_search, R.id.iv_more, R.id.tv_scan, R.id.tv_search, R.id.tv_start_chat, R.id.tv_unread, R.id.tv_read, R.id.tv_clear, R.id.iv_back, R.id.lly_more, R.id.tv_scan_chat, R.id.tv_search_chat, R.id.tv_start_group_chat, R.id.tv_add_friend, R.id.tv_search_group, R.id.tv_more_chat, R.id.iv_back_chat, R.id.lly_chat_more})
+    public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.iv_toolbar_search:
-                if (mTabLayout.getSelectedTabPosition() == 0) {
+                if (tabLayout.getSelectedTabPosition() == 0) {
                     startActivity(SearchChatRecordActivity.class);
                 }
-                if (mTabLayout.getSelectedTabPosition() == 1) {
+                if (tabLayout.getSelectedTabPosition() == 1) {
                     startActivity(SearchContactActivity.class);
                 }
                 break;
             case R.id.iv_more:
-                if (mTabLayout.getSelectedTabPosition() == 2){
+                if (tabLayout.getSelectedTabPosition() == 2) {
                     startActivity(SettingActivity.class);
-                }else {
+                } else {
                     openPanelView();
                 }
                 break;
@@ -212,47 +172,90 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
                 closePanelView();
                 break;
             case R.id.tv_scan:
-                startActivity(ScanActivity.class);
-                finish();
+                Intent goRegIntent = new Intent(this, ScanActivity.class);
+                ScreenManager.getScreenManager().StartPage(this, goRegIntent, true);
                 break;
             case R.id.tv_search:
-                ToastUtils.showMessage(mContext,"正在修改中...");
+                ToastUtils.showMessage(mContext, "正在修改中...");
                 break;
             case R.id.tv_start_chat:
-                ToastUtils.showMessage(mContext,"正在修改中...");
+                ToastUtils.showMessage(mContext, "正在修改中...");
                 break;
             case R.id.tv_unread:
-                ToastUtils.showMessage(mContext,"正在修改中...");
+                ToastUtils.showMessage(mContext, "正在修改中...");
                 break;
             case R.id.tv_read:
-                ToastUtils.showMessage(mContext,"正在修改中...");
+                ToastUtils.showMessage(mContext, "正在修改中...");
                 break;
             case R.id.tv_clear:
-                ToastUtils.showMessage(mContext,"正在修改中...");
+                ToastUtils.showMessage(mContext, "正在修改中...");
                 break;
             case R.id.tv_scan_chat:
-                startActivity(ScanActivity.class);
-                finish();
+                Intent intent = new Intent(this, ScanActivity.class);
+                ScreenManager.getScreenManager().StartPage(this, intent, true);
                 break;
             case R.id.tv_search_chat:
-                ToastUtils.showMessage(mContext,"正在修改中...");
+                ToastUtils.showMessage(mContext, "正在修改中...");
                 break;
             case R.id.tv_add_friend:
 //                ToastUtils.showMessage(mContext,"正在修改中...");
                 startActivity(AddFriendActivity.class);
                 break;
             case R.id.tv_search_group:
-                ToastUtils.showMessage(mContext,"正在修改中...");
+                ToastUtils.showMessage(mContext, "正在修改中...");
                 break;
             case R.id.tv_start_group_chat:
                 startActivity(CreatGroupActivity.class);
                 finish();
                 break;
             case R.id.tv_more_chat:
-                ToastUtils.showMessage(mContext,"正在修改中...");
+                ToastUtils.showMessage(mContext, "正在修改中...");
                 break;
         }
     }
 
-
+    //弹出更多
+    private void openPanelView() {
+        if (tabLayout.getSelectedTabPosition() == 0) {
+            llyMore.setVisibility(View.VISIBLE);
+            tvScan.startAnimation(mButtonInAnimation);
+            tvSearch.startAnimation(mButtonInAnimation);
+            tvStartChat.startAnimation(mButtonInAnimation);
+            tvRead.startAnimation(mButtonInAnimation);
+            tvUnread.startAnimation(mButtonInAnimation);
+            tvClear.startAnimation(mButtonInAnimation);
+        } else if (tabLayout.getSelectedTabPosition() == 1) {
+            llyChatMore.setVisibility(View.VISIBLE);
+            tvScanChat.startAnimation(mButtonInAnimation);
+            tvSearchChat.startAnimation(mButtonInAnimation);
+            tvStartGroupChat.startAnimation(mButtonInAnimation);
+            tvAddFriend.startAnimation(mButtonInAnimation);
+            tvSearchGroup.startAnimation(mButtonInAnimation);
+            tvMoreChat.startAnimation(mButtonInAnimation);
+        }
+    }
+    //收起更多
+    private void closePanelView() {
+        if (tabLayout.getSelectedTabPosition() == 0) {
+            tvScan.startAnimation(mButtonOutAnimation);
+            tvSearch.startAnimation(mButtonOutAnimation);
+            tvStartChat.startAnimation(mButtonOutAnimation);
+            tvRead.startAnimation(mButtonOutAnimation);
+            tvUnread.startAnimation(mButtonOutAnimation);
+            tvClear.startAnimation(mButtonOutAnimation);
+        } else if (tabLayout.getSelectedTabPosition() == 1) {
+            tvScanChat.startAnimation(mButtonOutAnimation);
+            tvSearchChat.startAnimation(mButtonOutAnimation);
+            tvStartGroupChat.startAnimation(mButtonOutAnimation);
+            tvAddFriend.startAnimation(mButtonOutAnimation);
+            tvSearchGroup.startAnimation(mButtonOutAnimation);
+            tvMoreChat.startAnimation(mButtonOutAnimation);
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
+    }
 }
