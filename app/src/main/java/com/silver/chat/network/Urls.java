8b5faba3c695efbeb8be1bf87864c92382b7b1ea
joe <maqiao@silversecuritytech.com>
2017-06-22 16:59:35 +0800

    图标修改

--- app/src/main/java/com/silver/chat/network/Urls.java
+++ app/src/main/java/com/silver/chat/network/Urls.java
@@ -5,7 +5,7 @@ package com.silver.chat.network;
  */
 
 public class Urls {
-//    public static final String SERVER = "http://192.254.1.5:7102/";
+//    public static final String SERVER = "http://192.254.1.21:7102/";
 //    public static final String SERVER = "http://192.254.1.7:7102/";
 //    public static final String SERVER = "http://192.168.10.215:7102/";
     public static final String SERVER = "http://imx.users.docker.sspaas.net/";
