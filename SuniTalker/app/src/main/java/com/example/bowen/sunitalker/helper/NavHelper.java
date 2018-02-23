7130e54a7379cbcf0d358b80f2a675fad8864854
bowen <zhangbowen@silversecuritytech.com>
2018-02-23 21:38:08 +0800

    主界面Fragment的调度与重用封装

--- SuniTalker/app/src/main/java/com/example/bowen/sunitalker/helper/NavHelper.java
+++ SuniTalker/app/src/main/java/com/example/bowen/sunitalker/helper/NavHelper.java
@@ -1,9 +1,11 @@
 package com.example.bowen.sunitalker.helper;
 
-import android.app.FragmentManager;
 import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.util.SparseArray;
-import android.widget.RelativeLayout;
+
 
 /**
  * 完成对Fragment的调度与重用问题，
@@ -78,32 +80,85 @@ public class NavHelper<T> {
             oldTab = currentTab;
             if (oldTab == tab) {
                 //如果说当前的Tab就是点击的Tab，那么我们不做处理
-                notifyReselect(tab);
+                notifyTabReselect(tab);
                 return;
             }
         }
         //赋值并调用切换方法
         currentTab = tab;
-        doTabChanged(currentTab,oldTab);
+        doTabChanged(currentTab, oldTab);
     }
 
-    private void doTabChanged(Tab<T> newTab,Tab<T> oldTab){
+    /**
+     * 进行Fragment的真实调度操作
+     *
+     * @param newTab 新的Tab<T>
+     * @param oldTab 旧的Tab<T>
+     */
+    private void doTabChanged(Tab<T> newTab, Tab<T> oldTab) {
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+
+        if (oldTab != null) {
+            if (oldTab.fragment != null) {
+                //从界面移除，但是还在Fragment的缓存空间中
+                ft.detach(oldTab.fragment);
+            }
+        }
+
+        if (newTab != null) {
+            if (newTab.fragment == null) {
+                //首次新建
+                Fragment fragment = Fragment.instantiate(context, newTab.clx.getName(), null);
+                //缓存起来
+                newTab.fragment = fragment;
+                //提交到FragmentManger
+                ft.add(containerId, fragment, newTab.clx.getName());
+            } else {
+                //从FragmentManger的缓存空间中重新加载到界面中
+                ft.attach(newTab.fragment);
+            }
+        }
+        //提交事务
+        ft.commit();
+        //通知回调
+        notifyTabSelect(newTab, oldTab);
 
     }
 
-    private void notifyReselect(Tab<T> tab){
+    /**
+     * 回调我们的监听器
+     *
+     * @param newTab 新的Tab<T>
+     * @param oldTab 旧的Tab<T>
+     */
+    private void notifyTabSelect(Tab<T> newTab, Tab<T> oldTab) {
+        if (listener != null) {
+            listener.onTabChanged(newTab, oldTab);
+        }
+    }
+
+    private void notifyTabReselect(Tab<T> tab) {
         // TODO 二次点击Tab所做的操作
     }
+
     /**
      * 我们的所有的Tab基础属性
      *
      * @param <T> 泛型的额外参数
      */
     public static class Tab<T> {
+        public Tab(Class<?> clx, T extra) {
+            this.clx = clx;
+            this.extra = extra;
+        }
+
         //Fragment对应的信息
         public Class<?> clx;
         //额外的字段，用户自己设定需要使用
         public T extra;
+        //内部缓存的对应的Fragment
+        //package权限外部无法使用
+        private Fragment fragment;
     }
 
     /**
